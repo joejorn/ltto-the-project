858f5b7bc105def722bf6b9f74d390ea5365534a
Kajorn Pathomkeerati <kajorn.pathomkeerati@student.kit.edu>
2017-06-09 16:47:47 +0200

    added sorting-function

--- src/app/visualization/shared/components/price-list/price-list.component.ts
+++ src/app/visualization/shared/components/price-list/price-list.component.ts
@@ -10,7 +10,7 @@ import { MdListItem } from '@angular/material';
   styleUrls: ['price-list.component.css']
 })
 
-export class PriceListComponent implements AfterViewInit, OnChanges {
+export class PriceListComponent implements OnChanges {
 
 	private loading: boolean;
 
@@ -25,6 +25,7 @@ export class PriceListComponent implements AfterViewInit, OnChanges {
     @Input() public sortProperty: string; // sort by property
     @Input() public sortDirection: string; // sort direction
     
+    @Input() public sorted: boolean;    // sort required
     @Input() public selectable: boolean;    // clickable
     @Input() public indexed: boolean;   // index before name
     
@@ -38,34 +39,63 @@ export class PriceListComponent implements AfterViewInit, OnChanges {
         this.maxItem = 0;  // display all items
         this.indexed = true;
 
+        this.sorted = false;
         this.sortProperty = 'name';
         this.sortDirection = 'ascending';
 
         this.selectedItem = new EventEmitter();
+
+        this.loading = true;
 	}
 
     ngOnChanges( changes: SimpleChanges ) {
 
-        if ( changes['items'] ) {
-			// pre-process: assign origin index
-			// let _items = changes['items'].currentValue;
-			// _items.forEach( (item:any, index: number) => { item['$index'] = index } )
-			// this._items = _items;
-        }
+        if ( changes['items']) {
 
+            // sort
+            if (this.sorted) {
+                this.sortItems();
+            }
+
+            if (this.items.length > 0 && this.loading) {
+                this.selectedItem.emit(this.items[this.selectedIndex]);
+                this.loading = false;
+            }
+            
+        } else if (changes['sortProperty']) {
+            this.sortItems();
+        }
     }
 
-	ngAfterViewInit() {
-		// in case of initial index
-		if (this.selectedIndex !== undefined ) {
-			/*let elemRef: ElementRef = this.listItems.toArray()[this.selectedIndex]['_element'];
-			let originIndex = elemRef.nativeElement.dataset.originIndex;
-			this.selectedItem.emit(this._items[originIndex]);*/
+    sortItems(): void {
+        let ascending = this.sortDirection === 'ascending';
+        this.items.sort(
+            (a,b) => {
+                let arr = [a,b].map( elem => {
+                    let val = elem[this.sortProperty];
+                    if (typeof val === 'number') {
+                        return val;
+                    } else if (!val) {
+                        return 0;
+                    } else {
+                        return ('' + val).toUpperCase();
+                    }
+                });
 
-			this.selectedItem.emit(this.items[this.selectedIndex]);
-		}
+                let newPos = 0; // default: no position change
+                if (arr[0] < arr[1]) {
+                    
+                    newPos = ascending ? -1 : 1;
 
-	}
+                } else if (arr[0] > arr[1]) {
+                    
+                    newPos = ascending ? 1 : -1;
+
+                }
+                return newPos;
+            }
+        );
+    }
 
     selectItem( index: number, item: any ) {
         if (this.selectable && index !== this.selectedIndex) {
